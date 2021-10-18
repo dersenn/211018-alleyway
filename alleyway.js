a07be073579d777c7e04d4e25fa48a12a666db7c
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-18 11:49:51 +0200

    implemented yLimit. moved stuff into functions.

--- alleyway.js
+++ alleyway.js
@@ -7,6 +7,9 @@ let score = 0
 //level up?
 let level = 1
 
+//yLimit for increased difficulty (limit playground)
+let yLimitTop = 0
+
 //ball parameters
 let xBall = 0
 let yBall = 0
@@ -15,15 +18,15 @@ let speedX = 2
 let speedY = 7
 
 //bar parameters
-let windowHeightBar = 10
-let windowWidthBar = 100
+let heightBar = 10
+let widthBar = 100
 let xBar
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
 
     // bar start
-    xBar = windowWidth/2 - windowWidthBar/2
+    xBar = windowWidth/2 - widthBar/2
 
     //ball start
     xBall = random(sizeBall/2,windowWidth-sizeBall/2)
@@ -32,6 +35,76 @@ function setup() {
     //score (text) parameters go here. size, position, font...
     textSize(60)
     textAlign(CENTER)
+}
+
+function drawLimit() {
+    push()
+    stroke(0,255,0,255)
+    line(0,yLimitTop,windowWidth,yLimitTop)
+    pop()
+}
+
+function checkScore() {
+    //increase score and speed. but only if game is on.
+    if (gameOn) {
+        score ++
+
+        //increase level after every 5 successful hits. and increase speed.
+        if (score % 3 == 0) {
+            // increase Level Count
+            level++
+            //speed acceleration
+            speedY += speedY*0.1
+            //make playground smaller
+            yLimitTop += heightBar
+        }
+    }
+}
+
+function makeBar() {
+    //bar move by arrow-keys
+    if (keyIsDown(LEFT_ARROW) && xBar > 0) {
+        xBar -= 10
+    }
+    if (keyIsDown(RIGHT_ARROW) && xBar < windowWidth - widthBar) {
+        xBar += 10
+    }
+    fill(255)
+    rect(xBar, windowHeight - heightBar, widthBar, heightBar)
+}
+
+function makeBall() {
+    //ball movements
+    //ball hits left/right wall
+    if (xBall > windowWidth - sizeBall/2 || xBall < sizeBall/2 ){
+        speedX = -speedX
+    }
+    //ball hits top wall
+    if (yBall < yLimitTop + sizeBall/2 ){
+        speedY = -speedY
+    }
+    //ball hits the bar, yay!
+    if (yBall > windowHeight - (sizeBall/2 + heightBar) 
+        && xBall > xBar 
+        && xBall < xBar + widthBar){
+            speedY = -speedY
+            checkScore()
+    }
+
+    //fail!
+    if (yBall > windowHeight - sizeBall/2){
+        speedY = 0
+        speedX = 0
+        fill(255,0,0)
+        yBall = windowHeight - sizeBall/4
+
+        //Game Over
+        gameOn = false
+    }
+    ellipse(xBall,yBall,sizeBall)
+
+    xBall += speedX
+    yBall += speedY
 }
 
 function draw() {
@@ -52,59 +125,13 @@ function draw() {
     text('Level: '+level, 5, 60)
     pop()
 
-    //bar move by arrow-keys
-    if (keyIsDown(LEFT_ARROW) && xBar > 0) {
-        xBar -= 10
-    }
-    if (keyIsDown(RIGHT_ARROW) && xBar < windowWidth - windowWidthBar) {
-        xBar += 10
-    }
+    //draw Limit
+    drawLimit()
 
-    fill(255)
-    rect(xBar, windowHeight - windowHeightBar, windowWidthBar, windowHeightBar)
+    //make Bar
+    makeBar()
 
+    //make Ball
+    makeBall()
 
-    //ball movements
-    //ball hits left/right wall
-    if (xBall > windowWidth - sizeBall/2 || xBall < sizeBall/2 ){
-        speedX = -speedX
-    }
-    //ball hits top wall
-    if (yBall < sizeBall/2 ){
-        speedY = -speedY
-    }
-    //ball hits the bar, yay!
-    if (yBall > windowHeight - (sizeBall/2 + windowHeightBar) 
-        && xBall > xBar 
-        && xBall < xBar + windowWidthBar){
-            speedY = -speedY
-
-            //increase score and speed. but only if game is on.
-            if (gameOn) {
-                score ++
-
-                //increase level after every 5 successful hits. and increase speed.
-                if (score % 5 == 0) {
-                    level++
-
-                    //speed acceleration
-                    speedY += speedY*0.1
-                }
-            }
-    }
-
-    //fail!
-    if (yBall > windowHeight - sizeBall/2){
-        speedY = 0
-        speedX = 0
-        fill(255,0,0)
-        yBall = windowHeight - sizeBall/4
-
-        //Game Over
-        gameOn = false
-    }
-    ellipse(xBall,yBall,sizeBall)
-
-    xBall += speedX
-    yBall += speedY
 }
