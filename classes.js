daf8b18e63338b9eb806b659ee72626fcccd61bf
dersenn <67318888+dersenn@users.noreply.github.com>
2021-10-18 13:46:26 +0200

    bricks created and placed.

---
+++ classes.js
@@ -0,0 +1,19 @@
+class Brick {
+    constructor(posX, posY, tW, tH, color) {
+        this.x = posX
+        this.y = posY
+        this.w = tW
+        this.h = tH
+        this.col = {r: color.r, g: color.g, b: color.b}
+    }
+
+    drawBrick() {
+        push()
+        stroke(255)
+
+        // console.log(this.x,this.y,this.w,this.h)
+        fill(this.col.r, this.col.g, this.col.b, 255)
+        rect(this.x,this.y,this.w,this.h)
+        pop()
+    }
+}
